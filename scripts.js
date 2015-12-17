a41bfb6bd2ce944417e4e19ca71555e0118dc203
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-12-17 10:27:19 +0100

    cleanup

--- scripts.js
+++ scripts.js
@@ -2,35 +2,6 @@ var UbkClient  = require('./node_modules/ubk/client/ws'),
     Class      = require('uclass'),
     Events     = require('uclass/events');
 
-function getAlbumArt (url){
-    var file = url;
-    
-    function callback() 
-    {
-        var artist = ID3.getTag(file, "artist");
-        var title = ID3.getTag(file, "title");
-        var album = ID3.getTag(file, "album");
-        
-        document.getElementById('id3_artist').innerHTML = artist;
-        document.getElementById('id3_title').innerHTML = title;
-        document.getElementById('id3_album').innerHTML = album;
-  };
-  
-  ID3.loadTags(url, callback, ["picture", "artist", "title", "album"]);
-}
-
-function register_command(ubk) {
-
-  console.log('register_command ?');
-
-    ubk.register_cmd('base', 'send_test', function(data){
-      alert('TESSSST');
-    });
-
-}
-
-getAlbumArt('Kalimba.mp3');
-
 var ubk = new UbkClient('http://172.19.21.59:8001');
 var ondeconnection = function() {
   setTimeout(function() {
@@ -46,10 +17,7 @@ var onconnection = function(ubk) {
 
 ubk.connect(function() { console.log('CONNECTED ?'); onconnection(ubk); } , ondeconnection);
 
-
-
 document.getElementById('test').addEventListener('click', function() {
-
   var callback = function() {
         console.log('its ok');
       },
