0b4af18b70239131df4677dc776e99968a44c114
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-12-17 13:07:15 +0100

    parse front code

--- scripts.js
+++ scripts.js
@@ -2,37 +2,95 @@ var UbkClient  = require('./node_modules/ubk/client/ws'),
     Class      = require('uclass'),
     Events     = require('uclass/events');
 
-var ubk = new UbkClient('http://172.19.21.59:8001');
-var ondeconnection = function() {
-  setTimeout(function() {
-    alert("connection lost");
-    document.location = document.location.href;
-  }, 100);
-};
 
-var onconnection = function(ubk) {
-  register_command(ubk);
-};
+var Client_Interface = new Class({
 
-function register_command(ubk) {
-  ubk.register_cmd('base', 'send_cmd', function(data){
-    console.log('ici', data);
-    if (!data.args)
-      return;
-    if (data.args.play) {
-      document.getElementById('video').play();
-    }
-  });
-}
+  Binds : ['onconnection', 'ondeconnection'],
 
-ubk.connect(function() { onconnection(ubk); } , ondeconnection);
+  initialize : function() {
+    var self = this;
 
-document.getElementById('play').addEventListener('click', function() {
-  var callback = function() {
-        console.log('its ok');
-      },
-      request = {
-        'play' : 'current'
-      };
-  ubk.send('base', 'send_cmd', request, callback);
+    self.server_url = document.location.hostname;
+
+    self.launch_ubk_client();
+
+  },
+
+  launch_ubk_client : function() {
+    self = this;
+    self.ubk = new UbkClient('http://' + self.server_url + ':8001');
+    self.ubk.connect(self.onconnection , self.ondeconnection);
+  },
+
+  onconnection : function() {
+    self = this;
+    self.register_command();
+    self.bind_interface();
+  },
+
+  ondeconnection : function() {
+    setTimeout(function() {
+      alert("connection lost");
+      document.location = document.location.href;
+    }, 100);
+  },
+
+  register_command : function() {
+    var self = this;
+
+    self.ubk.register_cmd('base', 'send_cmd', function(data){
+      console.log('ici', data);
+      if (!data.args)
+        return;
+      if (data.args.play) {
+        document.getElementById('video').play();
+      }
+      if (data.args.switch) {
+        document.getElementById('video').src = "./outputs/input_mpg.mp4";
+        document.getElementById('video').play();
+      }
+      if (data.args.go_to) {
+        document.getElementById('video').currentTime = data.args.go_to;
+        document.getElementById('video').play();
+      }
+    });
+  },
+
+  bind_interface : function() {
+    var self = this;
+    var dom = document;
+    dom.getElementById('play').addEventListener('click', function() {
+      var callback = function() {
+            console.log('its ok');
+          },
+          request = {
+            'play' : 'current'
+          };
+      self.ubk.send('base', 'send_cmd', request, callback);
+    });
+
+    dom.getElementById('switch').addEventListener('click', function() {
+      var callback = function() {
+            console.log('its ok');
+          },
+          request = {
+            'switch' : true
+          };
+      self.ubk.send('base', 'send_cmd', request, callback);
+    });
+
+    dom.getElementById('go_to').addEventListener('click', function() {
+      var callback = function() {
+            console.log('its ok');
+          },
+          request = {
+            'go_to' : 20
+          };
+      self.ubk.send('base', 'send_cmd', request, callback);
+    });
+
+  }
+
 });
+
+var ui_controller = new Client_Interface();
